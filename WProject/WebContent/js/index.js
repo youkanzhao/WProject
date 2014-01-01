98cea2400afb35abc458724cb2e4c56619344fa8
youkanzhao <youkanzhao@gmail.com>
2014-01-01 21:08:25 +0800

    saasdsd

--- WProject/WebContent/js/index.js
+++ WProject/WebContent/js/index.js
@@ -1,3 +1,24 @@
+function HomePage(){
+	this.init();
+}
+
+HomePage.prototype = {
+	init : function(){
+		this._bindEvent();
+	},
+	_bindEvent : function(){
+		$("#cygp-box .tabs-box li").click(function(){
+			var data = $(this).attr("data");
+			$("#cygp-box .tabs-box li").removeClass("sel-li");
+			$(this).addClass("sel-li");
+			$("#cygp-box .cygp-box-content").hide();
+			$("#cygp-box #" + data).show();
+			
+		});
+	}
+};
+
 $(function(){
 	//0.7196064652143359
+	var page = new HomePage();
 });
